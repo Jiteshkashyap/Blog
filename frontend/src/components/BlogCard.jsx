0fe5aa01fc5480f69ebf8eebf095ab03bc269390
Jiteshkashyap <94862516+Jiteshkashyap@users.noreply.github.com>
2025-09-26 16:23:47 +0530

    Blog changes

--- frontend/src/components/BlogCard.jsx
+++ frontend/src/components/BlogCard.jsx
@@ -10,12 +10,12 @@ const BlogCard = ({blog})=>{
 
     return(
         <div className="bg-white dark:bg-gray-800 dark:border-gray-600 p-5 rounded-2xl shadow-lg border hover:scale-105 transition-all">
-            <img src={blog.thumbnail} alt="" className="rounded-lg"/>
+            <img src={blog?.thumbnail} alt="" className="rounded-lg"/>
             <p className="text-sm mt-2 ">
-                By {blog.author.firstName} | {blog.category}  |  {formattedDate}
+                By {blog?.author?.firstName} | {blog?.category}  |  {formattedDate}
             </p>
-            <h2 className="text-xl font-semibold">{blog.title}</h2>
-            <h3 className="text-gray-500 mt-1">{blog.subTitle}</h3>
+            <h2 className="text-xl font-semibold">{blog?.title}</h2>
+            <h3 className="text-gray-500 mt-1">{blog?.subTitle}</h3>
             <Button onClick={()=>navigate(`/blogs/${blog._id}`)}className="mt-4 px-4 py-2 rounded-lg text-sm">
                 Read More
             </Button>
